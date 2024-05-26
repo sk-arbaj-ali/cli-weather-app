b2183c1d966b9d34053d7c76bb19393d3213d4bd
SK ARBAJ ALI <skarbajali.one@outlook.com>
2024-05-26 09:00:37 +0530

    feature update : existing feature updated and improved the exception handling.

--- main.py
+++ main.py
@@ -2,19 +2,57 @@ import typer
 import os
 import requests
 from dotenv import load_dotenv
-from constants import URL,CURRENT_WEATHER
+from constants import URL,CURRENT_WEATHER,FORECAST
 from typing_extensions import Annotated
 from utilities import print_weather_data
+from requests.exceptions import RequestException, ConnectionError
 
 
 app = typer.Typer(no_args_is_help=True)
 load_dotenv()
 
 @app.command()
+def forecast_weather(
+    city:Annotated[str, typer.Option(prompt="Provide the city name ",help="Provide city name")],
+    days:Annotated[int, typer.Option(prompt="Number of days of weather forecast. Value ranges from 1 to 10")]=1,
+    aqi:Annotated[str, typer.Option(help="If you want air quality data provide 'yes'")] = "no"
+    ):
+    '''
+    Use this command to check weather forecast data.
+    provide city, days and aqi information.
+    '''
+    try:
+        response = requests.get(
+        f"{URL}{FORECAST}",
+        params={"key":os.getenv("API_KEY"),"q":city,"days":days,"aqi":aqi}
+        )
+        response.raise_for_status()
+        json_response = response.json()
+        location_data = json_response["location"]
+        current_data = json_response["current"]
+        forecastday = json_response["forecast"]["forecastday"]
+        print_weather_data(location_data,current_data,forecastday)
+    except requests.HTTPError:
+        json_response = response.json()
+        print(f"Response status code : {response.status_code}")
+        print(f"error : {json_response['error']['message']}")
+    except ConnectionError:
+        print(f"error : a connection error has occurred.")
+        print(f"remarks : please check your internet connection.")
+    except RequestException as err:
+        print(f"error : Some error has occurred.")
+        print(f"remarks : please contact to developers.")
+
+
+@app.command()
 def current_weather(
     city:Annotated[str, typer.Option(prompt="Provide the city name ",help="Provide city name")],
     aqi:Annotated[str, typer.Option(help="If you want air quality data provide 'yes'")] = "no"
     ):
+    '''
+    Use this command to check the current weather.
+    Provide the city name and aqi information.
+    '''
     try:
         response = requests.get(
         f"{URL}{CURRENT_WEATHER}",
@@ -29,6 +67,18 @@ def current_weather(
         json_response = response.json()
         print(f"Response status code : {response.status_code}")
         print(f"error : {json_response['error']['message']}")
+    except ConnectionError:
+        print(f"error : a connection error has occurred.")
+        print(f"remarks : please check your internet connection.")
+    except RequestException as err:
+        print(f"error : Some error has occurred.")
+        print(f"remarks : please contact to developers.")
+
+@app.callback()
+def main():
+    '''
+        This is a cli weather app for realtime weather data.
+    '''
 
 if __name__ == "__main__":
     app()
--- utilities.py
+++ utilities.py
@@ -1,18 +1,44 @@
+from rich.console import Console
+from rich.table import Table
 
-def print_weather_data(location,current):
-    print(f"Location name : {location['name']}")
-    print(f"Region name : {location['region']}")
-    print(f"Country name : {location['country']}")
-    print(f"TimeZone : {location['tz_id']}")
-    print(f"Weather last-updated : {current['last_updated']}")
-    print(f"Temperature in celcius : {current['temp_c']}")
-    print(f"Temperature in farenheight : {current['temp_f']}")
-    print(f"Weather condition : {current['condition']['text']}")
-    print(f"Wind direction and speed(kph) : {current['wind_dir']} {current['wind_kph']}")
-    print(f"Humidity : {current['humidity']}")
+def print_weather_data(location,current,forecastday=None):
+
+    if forecastday:
+        console = Console()
+        print(f"Location name : {location['name']}")
+        print(f"Region name : {location['region']}")
+        print(f"Country name : {location['country']}")
+        print(f"TimeZone : {location['tz_id']}")
+        if current.get("air_quality", None) :
+            print("----- Air Quality Data -----")
+            print(f"Carbon Monoxide (μg/m3) : {current.get('air_quality')['co']}")
+            print(f"Ozone (μg/m3) : {current.get('air_quality')['o3']}")
+            print(f"Nitrogen dioxide (μg/m3) : {current.get('air_quality')['no2']}")
+        table = Table("Date","MaxTemp(Celsius)","MinTemp(Celsius)","MaxTemp(Fahrenheit)","MinTemp(Fahrenheit)",\
+                      "Wind(kph)","Humidity","Weather Condition"\
+                    )
+        for item in forecastday:
+            table.add_row(item["date"],str(item["day"]["maxtemp_c"]),str(item["day"]["mintemp_c"]),str(item["day"]["maxtemp_f"]),\
+                          str(item["day"]["mintemp_f"]),str(item["day"]["maxwind_kph"]),str(item["day"]["avghumidity"]),\
+                          item["day"]["condition"]["text"]
+                        )
+        console.print(table)
     
-    if current.get("air_quality", None) :
-        print(f"Carbon Monoxide (μg/m3) : {current.get('air_quality')['co']}")
-        print(f"Ozone (μg/m3) : {current.get('air_quality')['o3']}")
-        print(f"Nitrogen dioxide (μg/m3) : {current.get('air_quality')['no2']}")
+    else:
+        print(f"Location name : {location['name']}")
+        print(f"Region name : {location['region']}")
+        print(f"Country name : {location['country']}")
+        print(f"TimeZone : {location['tz_id']}")
+        print(f"Weather last-updated : {current['last_updated']}")
+        print(f"Temperature in celcius : {current['temp_c']}")
+        print(f"Temperature in farenheight : {current['temp_f']}")
+        print(f"Weather condition : {current['condition']['text']}")
+        print(f"Wind direction and speed(kph) : {current['wind_dir']} {current['wind_kph']}")
+        print(f"Humidity : {current['humidity']}")
+        
+        if current.get("air_quality", None) :
+            print("----- Air Quality Data -----")
+            print(f"Carbon Monoxide (μg/m3) : {current.get('air_quality')['co']}")
+            print(f"Ozone (μg/m3) : {current.get('air_quality')['o3']}")
+            print(f"Nitrogen dioxide (μg/m3) : {current.get('air_quality')['no2']}")
 
